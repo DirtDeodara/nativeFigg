0dc2983308a6f9347c092d3f5550d16759d12f54
DirtDeodara <dirtdeodara@gmail.com>
2023-04-11 08:50:14 -0500

    moved navigation container to index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
+import { NavigationContainer } from "@react-navigation/native";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+
+const AppWithNavigation = () => (
+  <NavigationContainer>
+    <App />
+  </NavigationContainer>
+);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(AppWithNavigation);
